7482100b868e6ae187be63d532f5054d796a8f03
Rajasekhar Kayyam <133310946+KayyamRaj@users.noreply.github.com>
2024-01-20 02:42:16 -0500

    Update SpringBootRestApiApplicationTests.java

--- src/test/java/com/raj/SpringBootRestApiApplicationTests.java
+++ src/test/java/com/raj/SpringBootRestApiApplicationTests.java
@@ -2,11 +2,12 @@ package com.raj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootApplication
-public class SpringBootRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestApiApplication.class, args);
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
 	}
 
 }
